81999b56af03489bd1fb587028bb4e01a6f129da
martin-montas <martinmontas1@gmail.com>
2024-11-05 18:10:19 -0500

    Final touches

--- parse-text-file.go
+++ parse-text-file.go
@@ -14,17 +14,17 @@ func run() {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Printf("%s[*]%s Current URL to scrape: %s\n",
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Printf("%s[*]%s Current URL to scrape: %s\n",
 			ColorBlue, ColorReset, line)
-		if *jsonFormat {
-			scrapeToJSON(line)
+			if *jsonFormat {
+				scrapeToJSON(line)
+			}
+			if !*jsonFormat {
+				scrapeToConsole(line)
+			}
 		}
-		if !*jsonFormat {
-			scrapeToConsole(line)
-		}
-	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
